7b8fac8180764cb7bddcf24c7428ff3897019390
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-08 22:17:31 -0500

    feat: example grade filter

--- src/courses/pages/courses.vue
+++ src/courses/pages/courses.vue
@@ -8,22 +8,24 @@
     <v-row>
       <v-col cols="12">
         <div class="d-flex flex-wrap justify-center mb-8">
-          <v-card v-for="course in courses" :key="course.id" class="pb-2 ma-3" width="290" max-height="450">
-            <div class="indigo accent-3">
-              <v-card-subtitle class="pb-0 white--text">
-                Course
-              </v-card-subtitle>
-              <v-card-title class="pb-1 pt-0 white--text font-weight-bold">
-                {{course.name}}
-              </v-card-title>
-            </div>
-            <v-card-text class="text-justify pb-1 text--secondary text-truncate">
-              <strong>Description:</strong> {{course.description}}
-            </v-card-text>
-            <v-card-actions>
-              <v-btn text color="indigo accent-4" class="font-weight-bold te" :to="`/courses/${course.id}`">see more</v-btn>
-            </v-card-actions>
-          </v-card>
+          <div v-for="course in courses" :key="course.id">
+            <v-card v-if="course.name=='Geography'" class="pb-2 ma-3" width="290" max-height="450">
+              <div class="indigo accent-3">
+                <v-card-subtitle class="pb-0 white--text">
+                  Course
+                </v-card-subtitle>
+                <v-card-title class="pb-1 pt-0 white--text font-weight-bold">
+                  {{course.name}}
+                </v-card-title>
+              </div>
+              <v-card-text class="text-justify pb-1 text--secondary text-truncate">
+                <strong>Description:</strong> {{course.description}}
+              </v-card-text>
+              <v-card-actions>
+                <v-btn text color="indigo accent-4" class="font-weight-bold te" :to="`/courses/${course.id}`">see more</v-btn>
+              </v-card-actions>
+            </v-card>
+          </div>
         </div>
       </v-col>
     </v-row>
