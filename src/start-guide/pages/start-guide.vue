95e19786173065759ec17e64d6e8d921d74e1b70
JulioSalazar12 <u202017572@upc.edu.pe>
2022-06-08 21:53:25 -0500

    feat: added base component start guide

---
+++ src/start-guide/pages/start-guide.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="start-guide">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: "start-guide",
+    data: () => ({
+
+    })
+}
+</script>
+
+<style>
+
+</style>
